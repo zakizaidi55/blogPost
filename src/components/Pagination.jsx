4f7d824dea680cb5d2226f9602f7eece448b1090
zakizaidi55 <zakizaidi55@gmail.com>
2023-11-14 14:33:11 +0530

    add next button and adding the css

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from '../context/AppContext'
 
 const Pagination = () => {
+  const {page, handlePageChange, totalPages} = useContext(AppContext);
   return (
-    <div>
+    <div className='w-full flex flex-row justify-center items-center border-2 fixed bottom-0 bg-white'>
+     <div className='flex justify-between w-11/12 max-w-[640px] py-2'>
+     <div className='flex gap-2'>
+      { page > 1 && 
+        (<button
+        className='rounded-md py-1 px-3 border-2 '
+        onClick={ () =>handlePageChange(page-1)}>
+          Previous
+          </button>)
+          
+        }
+
+        {
+          page < totalPages &&
+          ( <button
+          className='rounded-md py-1 px-3 border-2 '
+          onClick={() => handlePageChange(page+1)}>
+            Next
+          </button>)
+        
+        }
+
+     </div>
       
+
+      <p className='font-bold text-sm'>
+        Page = {page} of {totalPages}
+      </p>
+     </div>
     </div>
   )
 }
